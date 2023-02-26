a96fd287f75203e3bd58975463f7a27b4b5ca1a7
David Yappeter <davidyap11les@gmail.com>
2023-02-26 21:31:23 +0700

    fix: bundle analyzer stopping build

--- webpack.prod.js
+++ webpack.prod.js
@@ -36,6 +36,9 @@ module.exports = merge(common, {
         },
       ],
     }),
-    new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
   ],
 });
