bcc835995b905c065dd7d96e97c6a410fe1f0f6f
David Yappeter <davidyap11les@gmail.com>
2023-02-26 18:47:05 +0700

    fix: missing import

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
